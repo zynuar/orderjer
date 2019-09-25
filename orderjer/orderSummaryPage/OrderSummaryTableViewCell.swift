59531ab5050aea546e01a283a3dc837f6a34d907
Izzaty <izzaty@Izzatys-iMac.local>
2019-09-25 12:20:52 +0800

    rearrange files and folders

---
+++ orderjer/orderSummaryPage/OrderSummaryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderSummaryTableViewCell.swift
+//  orderjer
+//
+//  Created by Izzaty on 25/09/2019.
+//  Copyright © 2019 Izzaty. All rights reserved.
+//
+
+import UIKit
+
+class OrderSummaryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
