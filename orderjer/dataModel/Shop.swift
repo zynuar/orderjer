47a17d1433e1394281c509012b2deb74ef0edf55
PPAS RATU SIX <ppasratusix@PPASs-iMac-6.local>
2019-09-28 11:02:35 +0800

    Update Shop Data Model

--- orderjer/dataModel/Shop.swift
+++ orderjer/dataModel/Shop.swift
@@ -1,17 +1,18 @@
 //
 //  Shop.swift
-//  
+//  orderjer
 //
-//  Created by Izzaty on 27/09/2019.
+//  Created by PPAS RATU SIX on 28/09/2019.
+//  Copyright © 2019 Izzaty. All rights reserved.
 //
 
 import UIKit
 
 class Shop {
-    
+//    shopName: "mcd", shopLocation: "Damansara"
     var shopName: String
     var shopLocation: String
-    
+
     init(shopName: String, shopLocation: String) {
         self.shopName = shopName
         self.shopLocation = shopLocation
